561d07760a5e44029387bdf17574381cd7349154
ariaamadeus <amadeus.aria@gmail.com>
2022-04-19 01:23:46 +0700

    Threshold

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from PyQt5 import QtGui, QtWidgets, uic
 
 from utils import img_converter as conv
 from utils.gray_scale import grayScale as gray
-from utils.threshold import monoChrome as mono
+from utils.threshold import monoChrome, truncate, toZero, toZeroInv, otsu
 from utils import histogram as histo
 from utils import filters
 from utils.blur import blur, gauss, median
@@ -13,8 +13,10 @@ from utils.blur import blur, gauss, median
 class Application(QtWidgets.QMainWindow):
     def __init__(self,uiPath):
         super().__init__()
-        uic.loadUi(uiPath, self)
+        uic.loadUi(uiPath, self) # uiPath = rootwindow.ui
 	
+        ''' Variabel'''
+        
         self.img = None
         self.lastImg = None
         self.filename = None
@@ -22,15 +24,50 @@ class Application(QtWidgets.QMainWindow):
         self.imgFormat = "rgb"
         self.convImg = []
         self.lastPath = os.getcwd()
+        self.threshold = 127
+        self.comboIndex = 0
+        
+        ''' Tombol '''
         
         #self.findChild(QtWidgets.QPushButton, ('openBut')).clicked.connect(self._openClicked)
         #self.findChild(QtWidgets.QPushButton, ('saveBut')).clicked.connect(self._saveConverted)
         self.findChild(QtWidgets.QAction, ('actionOpen_Image')).triggered.connect(self._openClicked) 
         self.findChild(QtWidgets.QAction, ('actionSave')).triggered.connect(self._saveConverted)
-        self.findChild(QtWidgets.QPushButton, ('convertBut')).clicked.connect(self._convert)
+        #self.findChild(QtWidgets.QPushButton, ('convertBut')).clicked.connect(self._convert)
+        convertBut = self.findChild(QtWidgets.QPushButton, ('convertBut'))
         self.findChild(QtWidgets.QPushButton, ('geserBut')).clicked.connect(self._geser)
         
+        convertBut.setVisible(False)
+        ''' Stacked Widgets (depend ke pilihan dari Combo Box)'''
+
+        #self.stackedWidget = self.findChild(QtGui.QStackedWidget, ('stackedWidget'))
+        self.stackedWidget = self.findChild(QtWidgets.QStackedWidget, ('stackedWidget'))
+
+        ''' Combo Box '''
+        
+        self.comboBox = self.findChild(QtWidgets.QComboBox, ('comboBox'))
+        
+        self.comboBox.currentIndexChanged.connect(self._changeComboBox)
+        
+        ''' List Widgets (nempel ke Stacked Widgets) '''
+        
         self.listWidget = self.findChild(QtWidgets.QListWidget, ('listWidget'))
+        self.listWidget_2 = self.findChild(QtWidgets.QListWidget, ('listWidget_2'))
+        self.listWidget_3 = self.findChild(QtWidgets.QListWidget, ('listWidget_3'))
+        self.listWidget_4 = self.findChild(QtWidgets.QListWidget, ('listWidget_4'))
+
+        self.listWidget.itemClicked.connect(self._convert)
+        self.listWidget_2.itemClicked.connect(self._convert)
+        self.listWidget_3.itemClicked.connect(self._convert)
+        self.listWidget_4.itemClicked.connect(self._convert)
+        
+        ''' Spin Box '''
+
+        self.spinBox_4 = self.findChild(QtWidgets.QSpinBox, ('spinBox_4'))
+
+        self.spinBox_4.valueChanged.connect(self._convertSpin)
+
+        ''' Label '''
         
         self.previewLabel = self.findChild(QtWidgets.QLabel, ('previewLabel'))
         self.showLabel = self.findChild(QtWidgets.QLabel, ('showLabel'))
@@ -39,11 +76,22 @@ class Application(QtWidgets.QMainWindow):
 
         self.previewLabel.setHidden(True)
         self.showLabel.setHidden(True)
+        
+        ''' Drag & drop gambar '''
+        
         #self.previewLabel.setAcceptDrops(True)
-        self.setAcceptDrops(True)
+        #self.setAcceptDrops(True)
+
+        ''' show '''
 
         self.show()
         #self.showMaximized()
+
+    def _changeComboBox(self, val):
+        self.comboIndex = val
+        self.stackedWidget.setCurrentIndex(val)
+        if val == 0:
+            self._convert()
 
     def _openClicked(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self, 
@@ -67,12 +115,16 @@ class Application(QtWidgets.QMainWindow):
                 return -1
         except:
             try:
-                if not self.convImg & geser:
+                if not self.convImg and geser:
                     self._print("Gambar belum dikonversi!")
                     return -2
             except:
                 pass
         return 0
+
+    def _convertSpin(self):
+        self.threshold = self.spinBox_4.value()
+        self._convert()
 
     def _convert(self):
         #-1 sudah dikonversi, -2: gambar belum dipilih
@@ -80,7 +132,16 @@ class Application(QtWidgets.QMainWindow):
         
         # Cek list terpilih
         try:
-            choosen = self.listWidget.currentItem().text()
+            if self.comboIndex == 0:
+                choosen = "Gray Scale"
+            if self.comboIndex == 1:
+                choosen = self.listWidget.currentItem().text()
+            if self.comboIndex == 2:
+                choosen = self.listWidget_2.currentItem().text()
+            if self.comboIndex == 3:
+                choosen = self.listWidget_3.currentItem().text()
+            if self.comboIndex == 4:
+                choosen = self.listWidget_4.currentItem().text()
         except:
             choosen = ''
         if choosen == "Gray Scale":
@@ -91,13 +152,6 @@ class Application(QtWidgets.QMainWindow):
                 self._print("Proses...")
                 self.convImg = gray(self.img, fromCV2 = True)
                 self.imgFormat = "g"
-        elif choosen == "Monochrome":
-            #if self.mode == '1':
-            #    self._print("Gambar sudah dalam bentuk Monochrome")
-            #    return -1
-            self._print("Proses...")
-            self.convImg = mono(self.img, fromCV2 = True)
-            self.imgFormat = "g"
         elif choosen == "Average Blur":
             self._print("Proses...")
             self.convImg = blur(self.img)
@@ -127,6 +181,26 @@ class Application(QtWidgets.QMainWindow):
             self._print("Proses...")
             self.convImg = filters.sobel(self.img)
             self.imgFormat = "g"
+        elif choosen == "Monochrome":
+            self._print("Proses...")
+            self.convImg = monoChrome(self.img, fromCV2 = True, threshold = self.threshold)
+            self.imgFormat = "g"
+        elif choosen == "Truncate":
+            self._print("Proses...")
+            self.convImg = truncate(self.img, threshold = self.threshold)
+            self.imgFormat = "g"
+        elif choosen == "To Zero":
+            self._print("Proses...")
+            self.convImg = toZero(self.img, threshold = self.threshold)
+            self.imgFormat = "g"
+        elif choosen == "To Zero Inverted":
+            self._print("Proses...")
+            self.convImg = toZeroInv(self.img, threshold = self.threshold)
+            self.imgFormat = "g"
+        elif choosen == "Otsu":
+            self._print("Proses...")
+            self.convImg = otsu(self.img, threshold = self.threshold)
+            self.imgFormat = "g"
         else:
             self._print("Mode belum dipilih!")
             return -2
@@ -136,7 +210,7 @@ class Application(QtWidgets.QMainWindow):
         return 0
 
     def _geser(self):
-        if self._cekGambar() < 0 : return -1
+        if self._cekGambar(geser = True) < 0 : return -1
         
         try:
             self.lastImg = self.img
@@ -178,6 +252,8 @@ class Application(QtWidgets.QMainWindow):
         else:
             self.previewLabel.setPixmap(QtGui.QPixmap.fromImage(image))
             self.previewLabel.setHidden(False)
+            if self.comboIndex == 0:
+                self._convert()
         self._print()
 
     def _print(self, text = ""):
--- utils/threshold.py
+++ utils/threshold.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from .gray_scale import grayScale as gray
 
-def monoChrome(img, fromCV2 = True, thresh = 127):
+def monoChrome(img, fromCV2 = True, threshold = 127):
     if fromCV2:
         try:
             channel = img.shape[2]
@@ -13,5 +13,21 @@ def monoChrome(img, fromCV2 = True, thresh = 127):
             grayImg = gray(img,fromCV2)
         elif channel == 1:
             grayImg = img
-        newImg = cv2.threshold(grayImg, thresh, 255, cv2.THRESH_BINARY)[1]
+        newImg = cv2.threshold(grayImg, threshold, 255, cv2.THRESH_BINARY)[1]
         return newImg
+
+def truncate(img, threshold = 127):
+    grayImg = gray(img, fromCV2 = True)
+    return cv2.threshold(grayImg, threshold, 255, cv2.THRESH_TRUNC)[1]
+
+def toZero(img, threshold = 127):
+    grayImg = gray(img, fromCV2 = True)
+    return cv2.threshold(grayImg, threshold, 255, cv2.THRESH_TOZERO)[1]
+
+def toZeroInv(img, threshold = 127):
+    grayImg = gray(img, fromCV2 = True)
+    return cv2.threshold(grayImg, threshold, 255, cv2.THRESH_TOZERO_INV)[1]
+
+def otsu(img, threshold = 127):
+    grayImg = gray(img, fromCV2 = True)
+    return cv2.threshold(grayImg, threshold, 255, cv2.THRESH_OTSU)[1]
